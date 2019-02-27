d4e8581f5f2b17e75178890d52a0e41564a6d847
Alberto Morales <cmtmorales24@gmail.com>
2019-02-27 22:39:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,9 @@ array.join
 elsif array.length==2 
 array.join(" and ")
 else
-array.map! do |e| e == array.last ? "and #{e}" : e 
+array.map! do |e| e == array.last ? "and #{e}" 
 x.<<e
+
 end 
 x 
 end
